c27d7b312a17f061910a80c98d4e8a0067b24eaa
RandomFractals <RandomFractals@users.noreply.github.com>
2023-02-17 13:29:50 -0600

    register sql preview serializer to reopen sql preview after vscode reload

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import { ExtensionContext } from 'vscode';
+import { SqlPreviewSerializer } from './views/sqlPreviewSerializer';
 import { activateSqlPreviewPanel } from './views/sqlPreview';
 import { activateDiagnostics } from './diagnostics';
 import { registerCommands } from './commands';
@@ -15,4 +16,7 @@ export function activate(context: ExtensionContext) {
   activateDiagnostics(context);
   activateSqlPreviewPanel(context);
   registerCommands(context);
+
+  // register sql preview serializer for restore on vscode reload
+  context.subscriptions.push(SqlPreviewSerializer.register(context));
 }
